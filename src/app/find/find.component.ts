6b0c0c5f8fba94f5075644d68639230ded39f49b
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-03 23:10:15 +0300

    More interactive to Find component added

--- src/app/find/find.component.ts
+++ src/app/find/find.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-find',
@@ -6,10 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find.component.scss']
 })
 export class FindComponent implements OnInit {
+  @Input() searchQuery: string = '';
 
-  constructor() { }
+  // TODO: Debug
+  isInstructionsOn: boolean = false;
+  //isInstructionsOn: boolean = true;
 
-  ngOnInit() {
+  isFiltersOn: boolean = false;
+  isSearchOn: boolean = false;
+  isSearchResultsOn: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  closeInstructions(): void {
+    // TODO: Save the state to permanent storage to avoid annoying
+    this.isInstructionsOn = false;
   }
 
+  closeFilters(): void {
+    this.isFiltersOn = false;
+  }
+
+  toggleFilters(): void {
+    this.isFiltersOn = !this.isFiltersOn;
+    this.closeSearch();
+
+    if (this.isFiltersOn) {
+      // TODO: Service.getFilters()
+    }
+  }
+
+  closeSearch(): void {
+    this.isSearchOn = false;
+    this.isSearchResultsOn = false;
+    this.searchQuery = '';
+  }
+
+  toggleSearch(): void {
+    this.isSearchOn = !this.isSearchOn;
+    this.closeFilters();
+  }
+
+  checkSearchInput(): void {
+    if (this.searchQuery.length > 1) {
+      // TODO: Service.getLocations(this.searchQuery)
+      this.isSearchResultsOn = true;
+    } else {
+      this.isSearchResultsOn = false;
+    }
+  }
+
+
+  cancelFilters(): void {
+    this.closeFilters();
+  }
+
+  saveFilters(): void {
+    // TODO: Service.setFilters()
+    this.closeFilters();
+  }
 }
